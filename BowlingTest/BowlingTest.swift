f23afcf0a2aefb5af5e79c75494a1d61a5eb7745
Mephrine <dthwrld@gmail.com>
2021-12-03 00:15:45 +0900

    test: Be spare when all the pins knocked down on the second bal

--- BowlingTest/BowlingTest.swift
+++ BowlingTest/BowlingTest.swift
@@ -10,6 +10,14 @@ import XCTest
 class BowlingTest: XCTestCase {
   func test_shouldBeStrikeWhenAllThePinsKnockedDownOnTheFirstBall() throws {
 		let frame: Frame = NormalFrame()
-		XCTAssertTrue(frame.score(of: Ball(knockedDownPin: 10) ) is Strike)
+		XCTAssertTrue(frame.score(of: Ball(knockedDownPin: 10)) is Strike)
   }
+	
+	func test_shouldBeSpareWhenAllThePinsKnockedDownOnTheSecondBall() throws {
+		let frame: Frame = NormalFrame()
+		let firstBall = frame.score(of: Ball(knockedDownPin: 7))
+		guard firstBall == nil else { fatalError() }
+		
+		XCTAssertTrue(frame.score(of: Ball(knockedDownPin: 3)) is Spare)
+	}
 }
