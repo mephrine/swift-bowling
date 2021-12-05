7d61ff046ef6f1c7c2727222c5f8edd44e4d3620
Mephrine <dthwrld@gmail.com>
2021-12-05 23:25:58 +0900

    test: Be frame 2 when frame 1 ends

--- BowlingTest/BowlingTest.swift
+++ BowlingTest/BowlingTest.swift
@@ -14,23 +14,31 @@ class BowlingTest: XCTestCase {
   }
 	
 	func test_shouldBeSpareWhenAllThePinsKnockedDownOnTheSecondBall() throws {
-		let firstFrame: Frame = NormalFrame()
-		let secondFrame = try firstFrame.score(of: Ball(knockedDownPin: 7))
+		let firstBall: Frame = NormalFrame()
+		let secondBall = try firstBall.score(of: Ball(knockedDownPin: 7))
 		
-		XCTAssertTrue(try secondFrame.score(of: Ball(knockedDownPin: 3)) is Spare)
+		XCTAssertTrue(try secondBall.score(of: Ball(knockedDownPin: 3)) is Spare)
 	}
 	
 	func test_shouldBeMissWhenAllThePinsnNotKnockedDownOnTheSecondBall() throws {
-		let firstFrame: Frame = NormalFrame()
-		let secondFrame = try firstFrame.score(of: Ball(knockedDownPin: 6))
+		let firstBall: Frame = NormalFrame()
+		let secondBall = try firstBall.score(of: Ball(knockedDownPin: 6))
 		
-		XCTAssertTrue(try secondFrame.score(of: Ball(knockedDownPin: 3)) is Miss)
+		XCTAssertTrue(try secondBall.score(of: Ball(knockedDownPin: 3)) is Miss)
 	}
 	
 	func test_shouldBeGutterWhenNoneOfThePinsnNotingKnockedDown() throws {
-		let firstFrame: Frame = NormalFrame()
-		let secondFrame = try firstFrame.score(of: Ball(knockedDownPin: 0))
+		let firstBall: Frame = NormalFrame()
+		let secondBall = try firstBall.score(of: Ball(knockedDownPin: 0))
 		
-		XCTAssertTrue(try secondFrame.score(of: Ball(knockedDownPin: 0)) is Gutter)
+		XCTAssertTrue(try secondBall.score(of: Ball(knockedDownPin: 0)) is Gutter)
+	}
+	
+	func test_shouldBeFrame2WhenFrame1Ends() throws {
+		let bowlingGame = BowlingGame()
+		bowlingGame.play(with: Ball(knockedDownPin: 0))
+		bowlingGame.play(with: Ball(knockedDownPin: 0))
+		
+		XCTAssertTrue(bowlingGame.frames.count == 2)
 	}
 }
